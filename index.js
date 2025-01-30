f4dc587ee9b2d7cc1202e8e6180e6ce53fa52cee
ga101989 <113102996+ga101989@users.noreply.github.com>
2025-01-30 11:03:31 +0100

    hamburger menu

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 
-const btnAjtAuPanier = document.getElementById('btn1');
-const btnAjtAuPanier2 = document.getElementById('btn2');
-const btnAjtAuPanier3 = document.getElementById('btn3');
-console.log(btnAjtAuPanier);
-console.log(btnAjtAuPanier2);
-console.log(btnAjtAuPanier3);
 
-btnAjtAuPanier.addEventListener('click', function() {
-    alert('Harry Potter 1 ajouté au panier !');
-});
 
-btnAjtAuPanier2.addEventListener('click', function() {
-    alert('Harry Potter 2 ajouté au panier !');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var sidebar = document.querySelector('.sidebars');
+    var sidebarOpen = document.querySelector('.sidebars-ouvert');
 
-btnAjtAuPanier3.addEventListener('click', function() {  
-    alert('Harry Potter 3 ajouté au panier !');
+    if (sidebar) {
+        sidebar.addEventListener('click', function() {
+            sidebar.classList.toggle('open');
+            sidebarOpen.classList.toggle('open');
+        });
+    }
 });
